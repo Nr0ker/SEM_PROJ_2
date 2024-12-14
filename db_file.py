aca9e047a112c1710082c46675176887bf73a26c
v3ka <dsubboteko@gmail.com>
2024-12-14 18:07:57 +0100

    add db

--- db_file.py
+++ db_file.py
@@ -3,19 +3,39 @@ import sqlite3
 
 def get_user_db():
     pass
-    # conn = sqlite3.connect('advertisement.db')
-    # conn.row_factory = sqlite3.Row
-    # return conn
+    conn = sqlite3.connect('users.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-
-
-def init_db():
+def get_product_db():
     pass
-    # conn = get_db()
-    # conn.execute('''
-    #     CREATE TABLE IF NOT EXISTS advertisement (
-    #         id INTEGER PRIMARY KEY,
-    #         title TEXT NOT NULL,
-    #         text TEXT NOT NULL,
-    #     )
-    # ''')
+    conn = sqlite3.connect('products.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_user_db():
+    conn = get_user_db()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL,
+            is_admin BOOLEAN NOT NULL DEFAULT 0
+        )
+    ''')
+
+
+def init_product_db():
+    conn = get_product_db()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            desc TEXT NOT NULL,
+            curr_price FLOAT  NOT NULL,
+            is_sailed TEXT NOT NULL,
+            photo LONGBLOB
+        )
+    ''')
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from db_file import init_product_db, init_user_db, get_product_db, get_user_db
+
+init_user_db()
+init_product_db()
 
 app = FastAPI()
 
